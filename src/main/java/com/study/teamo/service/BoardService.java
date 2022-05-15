5646c8702037c0d1b2e3fdfc0ac3f1fddd045ece
songhee <1586piano@naver.com>
2022-05-15 23:48:45 +0900

    createBoard 메소드 추가

--- src/main/java/com/study/teamo/service/BoardService.java
+++ src/main/java/com/study/teamo/service/BoardService.java
@@ -1,2 +1,23 @@
-package com.study.teamo.service;public class BoardService {
+package com.study.teamo.service;
+
+import com.study.teamo.domain.Board;
+import com.study.teamo.dto.BoardDto;
+import com.study.teamo.dto.CreateBoardDto;
+import com.study.teamo.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+  @Autowired
+  private final BoardRepository boardRepository;
+
+  public BoardDto createBoard(CreateBoardDto request) {
+    Board board = new Board(request.getTitle(), request.getContent());
+    boardRepository.save(board);
+    return BoardDto.from(board);
+  }
 }
