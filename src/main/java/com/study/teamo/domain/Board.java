be678a3c4f3e7823a71b591e825695bba9143880
songhee <1586piano@naver.com>
2022-05-15 23:26:27 +0900

    Board Entity 추가

---
+++ src/main/java/com/study/teamo/domain/Board.java
@@ -0,0 +1,30 @@
+package com.study.teamo.domain;
+
+import com.fasterxml.jackson.databind.ser.Serializers.Base;
+import com.study.teamo.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name="BOARD")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TITLE", length = 100, nullable = false)
+    private String title;
+
+    @Column(name = "CONTENTS", length = 1000)
+    private String contents;
+}
+
