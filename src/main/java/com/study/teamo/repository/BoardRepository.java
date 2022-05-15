35fc6095d699ff50ada2518b51008d156c708e45
songhee <1586piano@naver.com>
2022-05-15 23:39:47 +0900

    BoardRepository 추가

--- src/main/java/com/study/teamo/repository/BoardRepository.java
+++ src/main/java/com/study/teamo/repository/BoardRepository.java
@@ -1,2 +1,10 @@
-package com.study.teamo.repository;public interface BoardRepository {
+package com.study.teamo.repository;
+
+import com.study.teamo.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Long, Board> {
+
 }
