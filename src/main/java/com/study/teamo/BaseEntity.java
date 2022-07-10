69fb410846a33091aaffe7007fa47150cbbd498a
songhee <1586piano@naver.com>
2022-07-10 18:47:40 +0900

    createdBy 및 컬럼 제약 조건 추가
    createdDateTime & createBy updatable false

--- src/main/java/com/study/teamo/BaseEntity.java
+++ src/main/java/com/study/teamo/BaseEntity.java
@@ -1,9 +1,11 @@
 package com.study.teamo;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -15,11 +17,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseEntity {
 
   @CreatedDate
+  @Column(updatable = false, nullable = false)
   private LocalDateTime createdDateTIme;
 
   @LastModifiedDate
+  @Column(nullable = false)
   private LocalDateTime lastModifiedTime;
 
   @LastModifiedBy
+  @Column(nullable = false)
   private String lastModifiedBy;
+
+  @CreatedBy
+  @Column(updatable = false, nullable = false)
+  private String createdBy;
 }
