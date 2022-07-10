794df07b034b67195bcbe4b2157c34d8a68ee5f5
songhee <1586piano@naver.com>
2022-07-10 18:51:02 +0900

    제약조건 추가

--- src/main/java/com/study/teamo/dto/board/UpdateBoardDto.java
+++ src/main/java/com/study/teamo/dto/board/UpdateBoardDto.java
@@ -1,10 +1,14 @@
 package com.study.teamo.dto.board;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UpdateBoardDto {
+
+  @NotNull(message = "title must not be null")
   private final String title;
+  @NotNull(message = "content must not be null")
   private final String content;
 
   public UpdateBoardDto(String title, String content) {
