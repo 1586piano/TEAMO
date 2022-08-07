70c8a86e3b54de06604920cde68acd62f72c253d
songhee <1586piano@naver.com>
2022-08-07 22:27:43 +0900

    게시물에 권한을 가진 사용자 이름 리스트 변수 명 수정

--- src/main/java/com/study/teamo/dto/board/BoardDto.java
+++ src/main/java/com/study/teamo/dto/board/BoardDto.java
@@ -17,27 +17,27 @@ public class BoardDto {
   private final LocalDateTime lastModifiedTime;
   private final String lastModifiedBy;
   private final String createdBy;
-  private final List<String> permissions;
+  private final List<String> authorizedUserNames;
 
   @Builder
   public BoardDto(String title, String content, LocalDateTime createdDateTIme,
       LocalDateTime lastModifiedTime, String lastModifiedBy, String createdBy,
-      List<String> permissions) {
+      List<String> authorizedUserNames) {
     this.title = title;
     this.content = content;
     this.createdDateTIme = createdDateTIme;
     this.lastModifiedTime = lastModifiedTime;
     this.lastModifiedBy = lastModifiedBy;
     this.createdBy = createdBy;
-    this.permissions = permissions == null ? new ArrayList<>() : permissions;
+    this.authorizedUserNames = authorizedUserNames == null ? new ArrayList<>() : authorizedUserNames;
   }
 
   public static BoardDto from(Board board) {
     return BoardDto.builder().title(board.getTitle()).content(board.getContent())
         .createdDateTIme(board.getCreatedDateTIme()).lastModifiedTime(board.getLastModifiedTime())
         .lastModifiedBy(board.getLastModifiedBy()).createdBy(board.getCreatedBy())
-        .permissions(board.getPermissions() == null ? new ArrayList<>()
-            : board.getPermissions().stream().map(bp -> bp.getUsers()).collect(
+        .authorizedUserNames(board.getPermissions() == null ? new ArrayList<>()
+            : board.getPermissions().stream().map(bp -> bp.getUserName()).collect(
                 Collectors.toList())).build();
   }
 }
