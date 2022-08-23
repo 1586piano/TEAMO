63cf0fbec4538822c3abfb517bb5cd7d2c5e26c5
songhee <1586piano@naver.com>
2022-08-23 19:11:43 +0900

    사용하지 않는 로직 제거

--- src/main/java/com/study/teamo/domain/board/BoardPermission.java
+++ src/main/java/com/study/teamo/domain/board/BoardPermission.java
@@ -45,9 +45,4 @@ public class BoardPermission {
   public String getUserName() {
     return this.user.getName();
   }
-
-  public void addPermission(Board board, User user) {
-    this.board = board;
-    this.user = user;
-  }
 }
