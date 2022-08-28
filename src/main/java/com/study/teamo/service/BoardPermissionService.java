786a6172fb207c72926498511102835abb1b789a
songhee <1586piano@naver.com>
2022-08-28 23:53:13 +0900

    BoardPermissionService 추가

--- src/main/java/com/study/teamo/service/BoardPermissionService.java
+++ src/main/java/com/study/teamo/service/BoardPermissionService.java
@@ -3,10 +3,12 @@ package com.study.teamo.service;
 import com.study.teamo.domain.auth.User;
 import com.study.teamo.domain.board.Board;
 import com.study.teamo.domain.board.BoardPermission;
-import com.study.teamo.dto.board.BoardDto;
+import com.study.teamo.dto.boardpermission.UpdateBoardPermissionDto;
+import com.study.teamo.dto.user.SimpleUserDto;
 import com.study.teamo.repository.BoardPermissionRepository;
 import com.study.teamo.repository.BoardRepository;
 import com.study.teamo.repository.UserRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -32,27 +34,28 @@ public class BoardPermissionService {
   private final BoardPermissionRepository boardPermissionRepository;
 
   @Transactional
-  public BoardDto addBoardPermissionToUsers(Long boardId, List<Long> users) {
+  public List<SimpleUserDto> addBoardPermissionToUsers(Long boardId, List<Long> users) {
     Board board = boardRepository.findById(boardId)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 보드입니다."));
 
+    List<SimpleUserDto> userDtoList = new ArrayList<>();
     for (Long userId : users) {
       User user = userRepository.findById(userId)
           .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+      userDtoList.add(SimpleUserDto.from(user));
       BoardPermission boardPermission = new BoardPermission(board, user);
       board.addPermission(boardPermission);
       boardPermissionRepository.save(boardPermission);
     }
-    return BoardDto.from(board);
+    return userDtoList;
   }
 
-  //TODO BoardPermission 수정 시, 삭제되는 Permission 제거 후 새로운 Permission 추가하도록 수정
   @Transactional
-  public void modifyBoardPermissionToUsers(Long boardId, List<Long> users) {
+  public List<SimpleUserDto> modifyBoardPermissionToUsers(Long boardId,
+      UpdateBoardPermissionDto request) {
     Board board = boardRepository.findById(boardId)
         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 보드입니다."));
 
-    //TODO 게시물 권한 수정은 생성자만 가능하도록 한다. 하지만 변화가 없더라도 update시 이 로직은 수행되게 되어 있음. 어떻게 반영할 것인지?
     if (board.getCreatedBy() != userDetailsServiceImpl.getCurrentUser().getName()) {
       throw new IllegalArgumentException("게시물 권한 수정은 생성자만 가능합니다.");
     }
@@ -64,22 +67,24 @@ public class BoardPermissionService {
         .collect(
             Collectors.toList());
     //board에 권한을 가질 새로운 사용자 ID 중복없이 추출
-    Set<Long> userIdsToBeGrantedPermissions = users.stream().collect(Collectors.toSet());
+    Set<Long> userIdsToBeGrantedPermissions = request.getUserPermissions().stream()
+        .collect(Collectors.toSet());
     //기존에 부여된, 삭제될 권한 추출 (새로 추가될 권한과 중복되는 것을 제외하고)
     alreadyUserIdsWithPermissions.removeAll(userIdsToBeGrantedPermissions);
     //기존에 부여된 권한 삭제
     alreadyUserIdsWithPermissions.stream().forEach(p -> boardRepository.deleteById(p));
 
-    addBoardPermissionToUsers(boardId, (List<Long>) userIdsToBeGrantedPermissions);
-//    System.out.println("board : " + boardPermissions.get(0).getBoard().getTitle());
-//    for (BoardPermission boardPermission : boardPermissions) {
-//      System.out.println(boardPermission.getUser());
-//    }
+    return addBoardPermissionToUsers(boardId,
+        (List<Long>) userIdsToBeGrantedPermissions);
   }
 
-  @Transactional
+  public List<SimpleUserDto> getPermissionedUserByBoardID(Long boardId) {
+    return boardPermissionRepository.getUserByBoardId(boardId).stream()
+        .map(u -> SimpleUserDto.from(u)).collect(
+            Collectors.toList());
+  }
+
   public List<Long> getPermissionedUserIdsByBoardID(Long boardId) {
     return boardPermissionRepository.getUserIdsByBoardId(boardId);
   }
-  //TODO : BoardPermission 삭제 추가
 }
