cef94d41dd60d368674e4515b6194fd65badfb53
songhee <1586piano@naver.com>
2022-08-28 23:52:35 +0900

    UpdateBoardPermissionDto 수정

--- src/main/java/com/study/teamo/dto/boardpermission/UpdateBoardPermissionDto.java
+++ src/main/java/com/study/teamo/dto/boardpermission/UpdateBoardPermissionDto.java
@@ -1,25 +1,16 @@
-package com.study.teamo.dto.board;
+package com.study.teamo.dto.boardpermission;
 
 import java.util.List;
-import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class UpdateBoardDto {
-
-  @NotNull(message = "title must not be null")
-  private final String title;
-  @NotNull(message = "content must not be null")
-  private final String content;
+public class UpdateBoardPermissionDto {
 
   private final List<Long> userPermissions;
 
   @Builder
-  public UpdateBoardDto(String title, String content,
-      List<Long> userPermissions) {
-    this.title = title;
-    this.content = content;
+  public UpdateBoardPermissionDto(List<Long> userPermissions) {
     this.userPermissions = userPermissions;
   }
 }
