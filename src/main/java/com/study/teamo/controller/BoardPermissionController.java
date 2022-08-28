46d046049905ce7760112f2575d0419b0b72f100
songhee <1586piano@naver.com>
2022-08-28 23:51:35 +0900

    BoardPermissionController get, update 추가

--- src/main/java/com/study/teamo/controller/BoardPermissionController.java
+++ src/main/java/com/study/teamo/controller/BoardPermissionController.java
@@ -1,16 +1,13 @@
 package com.study.teamo.controller;
 
-import com.study.teamo.dto.board.BoardDto;
-import com.study.teamo.dto.board.CreateBoardDto;
-import com.study.teamo.dto.board.UpdateBoardDto;
-import com.study.teamo.service.BoardService;
+import com.study.teamo.dto.boardpermission.UpdateBoardPermissionDto;
+import com.study.teamo.dto.user.SimpleUserDto;
+import com.study.teamo.service.BoardPermissionService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,34 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/permission")
+public class BoardPermissionController {
 
   @Autowired
-  private final BoardService boardService;
-
-  @PostMapping("")
-  public BoardDto createBoard(@RequestBody CreateBoardDto request) {
-    return boardService.createBoard(request);
-  }
-
-  @GetMapping("")
-  public List<BoardDto> getBoard() {
-    return boardService.getBoards();
-  }
+  private final BoardPermissionService boardPermissionService;
 
   @GetMapping("/{id}")
-  public BoardDto getBoard(@PathVariable("id") Long id) {
-    return boardService.getBoard(id);
-  }
-
-  @DeleteMapping("/{id}")
-  public void deleteBoard(@PathVariable("id") Long id) {
-    boardService.deleteBoard(id);
+  public List<SimpleUserDto> getBoardPermission(@PathVariable("id") Long id) {
+    return boardPermissionService.getPermissionedUserByBoardID(id);
   }
 
   @PutMapping("/{id}")
-  public BoardDto updateBoard(@PathVariable("id") Long id, @RequestBody UpdateBoardDto request) {
-    return boardService.updateBoard(id, request);
+  public List<SimpleUserDto> updateBoardPermission(@PathVariable("id") Long id,
+      @RequestBody UpdateBoardPermissionDto request) {
+    return boardPermissionService.modifyBoardPermissionToUsers(id, request);
   }
 }
